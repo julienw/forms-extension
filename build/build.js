12036e768b13d1e78a8af3e4d360c1c626b8162b
Julien Wajsberg <felash@gmail.com>
2015-07-26 12:06:42 +0200

    build script that works

--- build/build.js
+++ build/build.js
@@ -2,9 +2,12 @@
 
 var semver = require('semver');
 var fs = require('fs');
+var cp = require('child_process');
 
 var NPM_PACKAGE = 'package.json';
 var ADDON_PACKAGE = 'src/package.json';
+var ADDON_SOURCES = 'src/';
+var OUTPUT_XPI = 'dist/forms-extension-latest.xpi'
 
 function readPackage(fileName) {
   var file_content = fs.readFileSync(fileName);
@@ -23,6 +26,27 @@ function incrementRelease(version, mode) {
   return semver.inc(version, mode, 'pre');
 }
 
+function git() {
+  var args = [].slice.call(arguments);
+  console.log('[git]', args.join(' '));
+  cp.execFileSync('git', args, { stdio: 'inherit' });
+}
+
+function jpm() {
+  var args = [].slice.call(arguments);
+  console.log('[jpm]', args.join(' '));
+  var result = cp.execFileSync(
+    '../node_modules/.bin/jpm',
+    args, { cwd: ADDON_SOURCES }
+  ).toString();
+
+  return result;
+}
+
+function findXpiName(jpmOutput) {
+  return jpmOutput.match(/[^ ]+\.xpi\b/)[0];
+}
+
 var operations = {
   version: function(mode) {
     mode = mode || 'prerelease';
@@ -39,8 +63,18 @@ var operations = {
     addonPackage.write();
     console.log('Written to %s', ADDON_PACKAGE);
     addonPackage = null;
+    console.log('Generating a new XPI...');
+    operations.dist();
+    console.log('Committing and tagging with git');
+    git('add', ADDON_PACKAGE, NPM_PACKAGE, OUTPUT_XPI);
+    git('commit', '-m', 'v' + newVersion);
+    git('tag', newVersion);
   },
   dist: function() {
+    var xpiResult = jpm('xpi');
+    var xpiName = findXpiName(xpiResult);
+    console.log('Renaming %s to %s.', xpiName, OUTPUT_XPI);
+    fs.rename(xpiName, OUTPUT_XPI);
   },
   help: function() { printHelp(); }
 };
