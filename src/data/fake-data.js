f931dd6a32dab1dc310f890820450f0732b0ee4e
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-01-10 14:38:36 +0100

    Add fake data

--- src/data/fake-data.js
+++ src/data/fake-data.js
@@ -1,1 +1,1 @@
-var fakeData = [{"start":"2016-12-23T00:00:00.000Z","end":"2016-12-26T00:00:00.000Z","comment":"RTT","hours": 12}];
+var fakeData = [{"start":"2016-12-23T00:00:00.000Z","end":"2016-12-26T00:00:00.000Z","comment":"RTT","hours": 12}, {"start":"2016-12-28T00:00:00.000Z","end":"2016-01-01T00:00:00.000Z","comment":"Sickness","hours": 24}];
