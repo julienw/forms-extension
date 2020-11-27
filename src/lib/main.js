976a3dafd399895d3ab7812d09f94a5a62589da0
Julien Wajsberg <felash@gmail.com>
2020-11-27 15:56:42 +0100

    Reorganize the file a bit to make it more understandable and maintainable

--- src/lib/main.js
+++ src/lib/main.js
@@ -2,66 +2,83 @@ const { browserAction, tabs } = browser;
 
 var ptoWebsiteUrl = 'https://pto.mozilla.org/mypto.php';
 
+/**
+ * This is the entry point for this extension: this registers the event handler
+ * for the button.
+ */
 function init() {
   browserAction.onClicked.addListener(generate);
 }
 
+/**
+ * This will orchestrate all necessary calls to display the final form in a tab.
+ *
+ * @returns {Promise<void>}
+ */
 async function generate() {
   try {
-    const holidays = await getAllHolidays(ptoWebsiteUrl);
+    const tab = await openTabAndAuthenticate(ptoWebsiteUrl);
+    const tableDocument = await extractPtoInformationFromTab(tab);
+
+    // Note to a future reader: I tried to reuse this tab to display the result,
+    // but this wasn't working at the first try (error "no receiving end" when
+    // sending the message) and didn't seem worth the effort to make it work as
+    // the current way works fine.
+    await tabs.remove(tab.id);
+
+    const holidays = await getAllHolidaysFromDocument(tableDocument);
     await showResults(holidays);
   } catch(e) {
-    console.error('Error while handling holidays', e);
+    console.error('Error while generating the holiday form', e);
   }
 }
 
 /**
- * Retrieve a document at this url, handling authentication in the process.
+ * Retrieve the PTO table from the tab.
  *
- * @returns {Promise.<Document>}
+ * @param {Tab} tab This is the tab where we'll get the string from.
+ * @returns {Promise<Document>}
  */
-async function getDocumentAtUrl(url) {
-  const tab = await openTabAndAuthenticate(url);
+async function extractPtoInformationFromTab(tab) {
   const tableHtmlString = await tabs.executeScript(
     tab.id,
     { code: "document.querySelector('table').outerHTML", runAt: "document_end" }
   );
-  await tabs.remove(tab.id);
   const parser = new DOMParser();
   return parser.parseFromString(tableHtmlString, 'text/html');
 }
 
 /**
- * Retrieve the mypto page and parses it to a JS object
+ * Parses the document and extract a well-structured object containing the
+ * holiday information.
  *
- * @returns {Promise.<Array.<{start, end, comment}>>}
+ * @param {ParentNode} Document or element or fragment containing the PTO information we need.
+ * @returns {Promise<Array<{start, end, comment}>>}
  */
-function getAllHolidays(url) {
-  return getDocumentAtUrl(url).then(document => {
-    // By specifying tbody we avoid the header row.
-    var lines = document.querySelectorAll('tbody > tr');
-    var holidays = Array.from(lines).map((line) => {
-      var txtStartDate = line.children[1].textContent;
-      var txtEndDate = line.children[2].textContent;
-      var txtComment = line.children[3].textContent;
+async function getAllHolidaysFromDocument(tableDocument) {
+  // By specifying tbody we avoid the header row.
+  var lines = tableDocument.querySelectorAll('tbody > tr');
+  var holidays = Array.from(lines).map((line) => {
+    var txtStartDate = line.children[1].textContent;
+    var txtEndDate = line.children[2].textContent;
+    var txtComment = line.children[3].textContent;
 
-      var startDate = new Date(txtStartDate + ' UTC');
-      var endDate = new Date(txtEndDate + ' UTC');
+    var startDate = new Date(txtStartDate + ' UTC');
+    var endDate = new Date(txtEndDate + ' UTC');
 
-      return { start: startDate, end: endDate, comment: txtComment };
-    });
-
-    return holidays;
-  }).catch(e => {
-    console.error('got error while retrieving document:', e);
-    throw e;
+    return { start: startDate, end: endDate, comment: txtComment };
   });
+
+  return holidays;
 }
 
 /**
- * Opens a pto.mozilla.org page in a tab to trigger authentication.
+ * Opens a pto.mozilla.org page in a tab to trigger authentication. The promise
+ * is resolved when the url is finally loaded, after authentication is
+ * sucessful. It is rejected if the user closes the tab before authentication.
  *
- * @returns Promise<Tab>
+ * @param {String} url
+ * @returns {Promise<Tab>}
  */
 async function openTabAndAuthenticate(url) {
   const tab = await tabs.create({ url });
@@ -99,6 +116,12 @@ async function openTabAndAuthenticate(url) {
   return tab;
 }
 
+/**
+ * This opens a new tab with the form and hands over the holiday data to it.
+ *
+ * @param {Array<{start, end, comment}} holidays
+ * @returns {Promise<void>}
+ */
 async function showResults(holidays) {
   const tab = await tabs.create({
     url: '/data/results.html',
